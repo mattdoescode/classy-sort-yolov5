85e2f9b9a7ed43363c47e5f757fabbc55f165ff0
Matthew Loewen <matthewlanimates@gmail.com>
2022-01-26 16:42:36 -0500

    display db results to 3d plot

--- combine-results.py
+++ combine-results.py
@@ -3,10 +3,12 @@ import psycopg2
 import sys
 import time
 
+import random
+from itertools import count
+import pandas as pd
 import matplotlib.pyplot as plt
-
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from matplotlib.animation import FuncAnimation
 
 
 from config import config
@@ -17,9 +19,78 @@ from config import config
 
 # """ Connect to the PostgreSQL database server """
 
-def run_program(opt, *args):
-    single_object_track, single_camera_track, video_size, save_results = \
-        opt.single_object_track, opt.single_camera_track, opt.video_size, opt.save_results
+
+#def run_program(opt, *args):
+
+def animate(i, para):
+
+    print(i)
+    print(tableNames)
+    # GET INFO FROM DB
+    # for tableName in tableNames:
+    #     try:
+    #         query = """
+    #             SELECT * FROM "{}"
+    #             WHERE frame = (SELECT MAX(frame) FROM "{}")
+    #         """.format(tableName, tableName)
+    #         cur.execute(query)
+    #         values = cur.fetchall()
+    #     except (Exception, psycopg2.DatabaseError) as error:
+    #         print(error)
+
+    #     centerPointX = (values[0][2] + values[0][4]) / 2
+    #     centerPointY = (values[0][3] + values[0][5]) / 2
+
+    #     #convert from 2d camera perspective to 3d point
+    #     if tableName[-1] == "0":
+    #         locationGlobal['x'] = centerPointX
+    #         locationGlobal['z'] = centerPointY
+    #     elif tableName[-1] == "1":
+    #         locationGlobal['x'] = (locationGlobal['x'] + centerPointX) / 2
+    #         locationGlobal['y'] = centerPointY
+    #     else:
+    #         print("Error with the camera")
+    
+    #     print("global location is (x,y,z):", locationGlobal['x'], locationGlobal['y'], locationGlobal['z'])
+ 
+
+    # NOW LETS DRAW THIS INFO
+    plt.cla()
+
+    ax.axes.set_xlim3d(left=0, right=50) 
+    ax.axes.set_ylim3d(bottom=0, top=50) 
+    ax.axes.set_zlim3d(bottom=0, top=50)
+
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+
+    print('running 2')
+    #scatter(x pos, y pos, z pos, s = size of plot point, label for legend)
+    ax.scatter(2,3,40, s = 500, label = "Fish 1")
+
+    print('running 3')
+    plt.legend(loc='upper left')
+    print('running 4')
+    plt.tight_layout()
+    print('running 5')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Object location determination")
+    parser.add_argument('--single_object_track', type=bool, default=True, help="Tracking 1 object")
+    parser.add_argument('--single_camera_track', type=bool, default=False, help="Tracking from 2 cameras")
+    parser.add_argument('--video_size', type=list, default=[640,480], help="Video frame dimensions")
+    parser.add_argument('--save_results', default=True, help="save results to a .txt file or not")
+    parser.add_argument('--fps', default=30, help="fps of camera(s)")
+
+    args = parser.parse_args()
+
+    print("RUNNING VISUALS with args:")
+    print(args)
+
+    # DO THE SETUP STUFF 
+    single_object_track, single_camera_track, video_size, save_results, fps = \
+        args.single_object_track, args.single_camera_track, args.video_size, args.save_results, args.fps
 
     conn = None
     try:
@@ -55,8 +126,7 @@ def run_program(opt, *args):
             print(error)
             sys.exit()
 
-    # we could start treading here... 
-    # for item in name:
+    # we could start tread(s) here? 
 
     tableNames = []    
     for item in enumerate(name):
@@ -71,50 +141,21 @@ def run_program(opt, *args):
     #when we have 2 cameras
     #camera 0 captures x,y - globally -> x,z
     #camera 1 captures x,y - globally -> x,y
+    # CONFIRM THIS INFO ^^^^ 1/26/21
+    locationGlobal['x'] = locationGlobal['y'] = locationGlobal['z'] = 0
 
-    while True:
-        locationGlobal['x'] = locationGlobal['y'] = locationGlobal['z'] = 0
-        for tableName in tableNames:
-            try:
-                query = """
-                    SELECT * FROM "{}"
-                    WHERE frame = (SELECT MAX(frame) FROM "{}")
-                """.format(tableName, tableName)
-                cur.execute(query)
-                values = cur.fetchall()
-            except (Exception, psycopg2.DatabaseError) as error:
-                print(error)
+    # setup init graph 
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(azim = -104, elev = 28)
 
-            centerPointX = (values[0][2] + values[0][4]) / 2
-            centerPointY = (values[0][3] + values[0][5]) / 2
+    #starts main loop of the program -> animate function does it all 
+    # ani = FuncAnimation(fig, animate, interval=1000)
+    ani = FuncAnimation(fig, animate, fargs=(tableNames,),interval=100)
 
-            #convert from 2d camera perspective to 3d point
-            if tableName[-1] == "0":
-                locationGlobal['x'] = centerPointX
-                locationGlobal['z'] = centerPointY
-            elif tableName[-1] == "1":
-                locationGlobal['x'] = (locationGlobal['x'] + centerPointX) / 2
-                locationGlobal['y'] = centerPointY
-            else:
-                print("Not sure how'd you ever end up here")
-            
-        print("global location is (x,y,z):", locationGlobal['x'], locationGlobal['y'], locationGlobal['z'])
-
-    # eventually thread getting the locations
-    # eventually thread updating visual 3D location of the fish 
-    # eventually thread updating VR screen for the fish
-    # while True:
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Object location determination")
-    parser.add_argument('--single-object-track', type=bool, default=True, help="Tracking 1 object")
-    parser.add_argument('--single-camera-track', type=bool, default=False, help="Tracking from 2 cameras")
-    parser.add_argument('--video-size', type=list, default=[640,480], help="Video frame dimensions")
-    parser.add_argument('--save-results', default=True, help="save results to a .txt file or not")
-
-    args = parser.parse_args()
-
-    print("RUNNING VISUALS with args:")
-    print(args)
-
-    run_program(args)
+    #plt.tight_layout()
+    print("running 7")
+    plt.show()
+    print("running 8")
+    
+    #run_program(args)
