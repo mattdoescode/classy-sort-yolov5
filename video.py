1bfd903818e70dcbea7fdf05a3f95d0fe602b545
Matthew Loewen <matthewlanimates@gmail.com>
2022-02-10 17:57:37 -0500

    quick math conversion + successful convert video mapping

--- video.py
+++ video.py
@@ -1,22 +1,65 @@
-from asyncio.windows_events import NULL
 import cv2
 import numpy as np
 
+""" "output resolution"
+
+tank measures 10 gal are
+
+20.25" length
+12.625" height
+10.5" width
+
+Used webcam records at 640 x 480
+
+    640
+-------------
+|           |
+|           | 480 
+|           |
+-------------
+
+assuming everything is perfect 
+mapping largest dimensions 
+640px = 20.25"
+
+1 pixel = 0.031640625 inches
+conversely 
+31.6049382716 pixels = 1 inch
+
+we will base all tracking results off of this conversion
+NEED TO MAKE SURE THIS SCALE IS KEPT FOR ALL CONVERSIONS
+
+SO....
+
+20.25 x 31.6049382716 = 640 -> Our X will be 640 pixels
+12.625 X 31.6049382716 = 399.012345679 -> Our Y for video will be 399 pixels 
+10.5 x 31.6049382716 = 331.851851852 -> Our Z will be 332 pixels
+
+As we track the fish we can use these coordinates to translte to real world location
+"""
+
 cap = cv2.VideoCapture(0)
 
+
+#point order 
+#TL, TR, BR, BL
 saved_points = []
+tank_points = [[0,0],[640,0],[640,399],[0,399]]
+# X, Y, Z
+scaledResolution = [640, 399, 332]
+# TO DO: write conversion function for dynamic tank size + camera resolution
 
-def mousePoints(event, x,y,flags,params):
+#record mouse clicks to create box for perspective mapping
+def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        # print(len(saved_points))
-        # print(x,y)
         saved_points.append([x,y])
-        # print(saved_points)
 
+#draw mouse clicks
 def drawPoints():
     for points in saved_points: 
         cv2.circle(frame, (points[0],points[1]), 5, (0,255),5)
 
+#make a box around points
 def connectPoints():
     previous = []
     first = []
@@ -27,26 +70,24 @@ def connectPoints():
             continue
         
         #last point draws to the first 
-        #last point dears to second last visited
+        #last point draws to second last visited
         if saved_points[-1] == point:
             cv2.line(frame,[point[0],point[1]],[first[0],first[1]],(0,200,0),3)
             cv2.line(frame,[previous[0],previous[1]],[point[0],point[1]],(0,200,0),3)
         else:
             cv2.line(frame,[previous[0],previous[1]],[point[0],point[1]],(0,200,0),3)
         previous = point
-
     previous = []
-            
+
+#start video capture
 ret, frame = cap.read()
 cv2.imshow('frame', frame)
 cv2.setMouseCallback("frame", mousePoints)
 
+#get tank points
 while len(saved_points) != 4: 
-
     ret, frame = cap.read()
-
     drawPoints()
-
     cv2.imshow('frame',frame)  
 
     if len(saved_points) == 4:
@@ -57,16 +98,26 @@ while len(saved_points) != 4:
     if cv2.waitKey(24) == 27:
         break
 
-#remove callback
+#stop recording mouse clicks
 cv2.setMouseCallback("frame",lambda *args : None)
+
+#calculate matrix for warping 
+print(saved_points)
+print(tank_points)
+matrix = cv2.getPerspectiveTransform(np.float32(saved_points), np.float32(tank_points))
 
 while 1:
     ret, frame = cap.read()
 
+    #apply matrix to new frame
+    result = cv2.warpPerspective(frame, matrix,(640,399))
+
+    #still draw bounding box on raw video capture
     drawPoints()
     connectPoints()
 
-    cv2.imshow('frame',frame)  
+    cv2.imshow('frame',frame)
+    cv2.imshow('conversion', result) # Transformed Capture
     if cv2.waitKey(24) == 27:
         break
 
