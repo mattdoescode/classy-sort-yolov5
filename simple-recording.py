c5dcd237556c53421b253c812b4078f207db29a3
Matthew Loewen <matthewlanimates@gmail.com>
2022-03-03 13:02:09 -0500

    fixed video speed up bug

--- simple-recording.py
+++ simple-recording.py
@@ -4,15 +4,16 @@ import cv2
  
 cap = cv2.VideoCapture(0)
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 24.0)
 
 width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+fps = cap.get(cv2.CAP_PROP_FPS)
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-videoWriter = cv2.VideoWriter('C:\\Users\\matt2\Desktop\\bs\\video.avi', fourcc, fps, (int(width),int(height)))
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+videoWriter = cv2.VideoWriter('C:\\Users\\matt2\Desktop\\bs\\video.mp4', fourcc, fps, (int(width),int(height)))
 
 ret, frame = cap.read()
 
