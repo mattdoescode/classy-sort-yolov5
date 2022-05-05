6989603b875f198fe51972accaec081190b6d2e6
Matthew Loewen <matthewlanimates@gmail.com>
2022-05-04 20:37:39 -0400

    file clean up. base work for merging files

--- display-results.py
+++ display-results.py
@@ -1,45 +1,81 @@
+"""
+1. This script grabs data from the postgreSQL DB. 
+2. It calculates the 3D location of each fish while flitering out poor detection data (if any)
+Each camera only tracks a fish based on a calculated 2D location. Initial pairings are based 
+on this. Subsequent pairings (tracking) is done via the ID's given from YOLO output. 
+3. Displays each detected fish on a 3D chart
+4. drives virtual reality screen 
+"""
+
+#camera 0 - front facing camera - captures x,y - globally -> x,z
+#camera 1 - top facing camera - captures x,y - globally -> x,y
+
 import argparse
 import psycopg2
 import sys
-import time
-
-import random
-from itertools import count
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-
+#db info
 from config import config
 
-# this script gets the information from postgres about both cameras
-# it them combines the data and makes a 3d map that follows the fish
-# this script is also the VR screen for the fish
+class detectedObject():
+    def __init__(self):
+        self.TopX1 = None
+        self.TopY1 = None
+        self.TopX2 = None
+        self.TopY2 = None
+        self.FrontX1 = None
+        self.FrontY1 = None
+        self.FrontX2 = None
+        self.FrontY2 = None
+        self.TopIDNum = None
+        self.FrontIDNum = None
+        self.TopFrame = None
+        self.FrontFrame = None
+        self.Completed = False
 
-# !!!!!!!!!!
-# assume that DB records ending with '0' are front facing camera
-# assume that DB records ending with '1' are top down facing camera
+def getTableView(tableName):
+    if "top" in tableName or "TOP" in tableName or "Top" in tableName:
+        return "top"
+    elif "front" in tableName or "FRONT" in tableName or "Front" in tableName:
+        return "front"
+    else:
+        print("error detecting camera view")
+        print("table name w/ error is: ", tableName)
+        sys.exit("check DB table name")
 
-# """ Connect to the PostgreSQL database server """
-
-#def run_program(opt, *args):
+#create temp object + add properties based on camera perspective
+def createTrackedObjectFromDBData(tableName, dbRecord):
+    currentTemp = detectedObject()
+    if "top" in tableName or "TOP" in tableName or "Top" in tableName:
+        currentTemp.TopX1 = dbRecord[2]
+        currentTemp.TopY1 = dbRecord[3]
+        currentTemp.TopX2 = dbRecord[4]
+        currentTemp.TopY2 = dbRecord[5]
+        currentTemp.TopFrame = dbRecord[1]
+        currentTemp.TopIDNum = dbRecord[7]
+    elif "front" in tableName or "FRONT" in tableName or "Front" in tableName:
+        currentTemp.FrontX1 = dbRecord[2]
+        currentTemp.FrontY1 = dbRecord[3]
+        currentTemp.FrontX2 = dbRecord[4]
+        currentTemp.FrontY2 = dbRecord[5]
+        currentTemp.FrontFrame = dbRecord[1]
+        currentTemp.FrontIDNum = dbRecord[7]
+    else:
+        print("ERROR - DB TABLE NAME REQUIRED TO HAVE 'FRONT' or 'TOP' in name")
+        sys.exit()
+    return currentTemp
 
 def animate(i, para):
+    tempDetected = []
+    #how many frames of no detection before we delete
+    decayRate = None
 
-    # print(i)
-    # print(tableNames)
-    # GET INFO FROM DB
-
-    # table names can have 4 endings
-    """
-    0 - front facing camera live capture
-    1 - top facing camera live capture
-    2 - front facing recorded
-    3 - top facing recorded
-    """
     for tableName in tableNames:
         try:
+            # get tracking results from newest frame
             query = """
                 SELECT * FROM "{}"
                 WHERE frame = (SELECT MAX(frame) FROM "{}")
@@ -49,57 +85,129 @@ def animate(i, para):
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
-        centerPointX = (values[0][2] + values[0][4]) / 2
-        centerPointY = (values[0][3] + values[0][5]) / 2
 
-        #convert from 2d camera perspective to 3d point
-        if tableName[-1] == "0" or tableName[-1] == "2":
-            locationGlobal['x'] = centerPointX
-            locationGlobal['z'] = centerPointY
-        elif tableName[-1] == "1" or tableName[-1] == "3":
-            locationGlobal['x'] = (locationGlobal['x'] + centerPointX) / 2
-            locationGlobal['y'] = centerPointY
-        else:
-            print("Error - unrecognized DB data")
-            sys.exit()
+        # Matching results from the 2 camera feeds is complicated
+        # lots of cases to consider (missing detections, fish 
+        # all close in apx location, etc) 
+        # once pairing is made we can track objects from their
+        # id output from YOLO + Sort. -> this makes things really easy
+
+        #each DB record is 
+        #recordID, frame#, x1, y1, x2, y2, detected object, sort item ID
+
+        
+        ######## MAKE FIRST INITIAL PAIRINGS
+
+        if tempDetected and len(tempDetected) == len(values):
+            #table view
+            currentTableView = getTableView(tableName)
     
-        print("global location is (x,y,z):", locationGlobal['x'], locationGlobal['y'], locationGlobal['z'])
- 
+        #if tempItems is empty and we have results from DB
+        if not tempDetected and len(values) > 0:
+            #for each value from DB create new object and populate it based on camera perspective 
+            for dbRecord in values:
+                currentTemp = createTrackedObjectFromDBData(tableName, values)
+                tempDetected.append(currentTemp)
+        
+        #if we have tempitems and results from DB 
+        #we need to merge db results with what we have from the other table
+        elif tempDetected and len(values) > 0:
+            pass
+            #match temp detected & current DB results
+            #here we have 1 or more record from both cameras
+            #similarPosMaxDiff
+            if not activelyTracked:
+                pass
+            else: 
+                #merge with regards to atively tracked records
+                pass
 
-    # NOW LETS DRAW THIS INFO
+
+
+            #use nearest neighbor algo 
+
+
+            #combining works (without references to past frames) by comparing the shared deminsion.
+            #we find detected objects that have the closest X deminsion location per camera.
+            #once objects from each camera are paired on the x we get the item # from the sort algo
+            #the item will then be paired by sort algo numbers
+            # if currectlyTracked:
+            #     #compare x locations
+            #     #case where result sets are different sizes
+                
+            #     #compareCompletedTemps with currectlyTracked
+            #     # update CurrentlyTracked
+            # else:
+            #     # output From combineTemps = currentlyTracked
+
+
+            # #if we currently have tracked objects we compare existing objects w/ their sort algo items. 
+            #     #if we have matching numbers we simply update the objects position
+            #     #if we have missmatch by id's
+            #         #if 1 missmatch id -> update the missing ID w/ new ID
+            #         #if multiple missmatch ids -> compare on shared X and update with saved item values 
+            # else:
+            #     print("need to combine and check currectly displayed")
+
+        #if tempitems and no results from DB
+        elif not tempDetected and len(values) <= 0:
+            print("missing information from 1 camera")
+        #if no tempitems and no results from DB
+        elif tempDetected and len(values) <= 0:
+            print("missing information from 1 camera")
+        
+        else:
+            print("something has gone horribly wrong.")
+            print("you should never be here")
+            sys.exit()
+
+    #find match in global detected objects 
+    #if no match can be found make new record
+
+    #cases
+    #0 temp records
+    #1 incomplete temp record
+    #n completed records with m incompleted records
+    print(tableName, values)
+    # PLOT FISH
+    #ax.scatter(x pos, y pos, z pos, s = size of plot point, label for legend)
+
+    #invert axis
+    # ax.invert_zaxis()
+    # ax.invert_yaxis()
+    # ax.invert_zaxis()
     plt.cla()
-
     ax.axes.set_xlim3d(left=0, right=1000) 
     ax.axes.set_ylim3d(bottom=0, top=1000) 
     ax.axes.set_zlim3d(bottom=0, top=1000)
-
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
-
-    #scatter(x pos, y pos, z pos, s = size of plot point, label for legend)
-    ax.scatter(int(locationGlobal['x']),int(locationGlobal['y']),int(locationGlobal['z']), s = 500, label = "Fish 1")
-
     plt.legend(loc='upper left')
     plt.tight_layout()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Object location determination")
+    parser = argparse.ArgumentParser(description="Display location of fish + drive 'vr' display")
+
+    #single object and single camera
     parser.add_argument('--single_object_track', type=bool, default=True, help="Tracking 1 object")
-    parser.add_argument('--single_camera_track', type=bool, default=False, help="Tracking from 2 cameras")
+    parser.add_argument('--single_camera_track', type=bool, default=False, help="Tracking from 1 camera")
+
     parser.add_argument('--video_size', type=list, default=[640,480], help="Video frame dimensions")
     parser.add_argument('--save_results', default=True, help="save results to a .txt file or not")
     parser.add_argument('--fps', default=30, help="fps of camera(s)")
 
     args = parser.parse_args()
 
-    print("RUNNING VISUALS with args:")
-    print(args)
+    print("starting with args:")
+    print("\t",args)
 
-    # DO THE SETUP STUFF 
+    # set variables from arguments
     single_object_track, single_camera_track, video_size, save_results, fps = \
         args.single_object_track, args.single_camera_track, args.video_size, args.save_results, args.fps
 
+    # Get table names
+    tableNames = []
     conn = None
     try:
         params = config()
@@ -107,50 +215,43 @@ if __name__ == '__main__':
         conn = psycopg2.connect(**params)
         conn.autocommit = True
         cur = conn.cursor()
+        #tracking from 1 camera or two
         if single_camera_track:
+            print("getting most recent DB save")
             query = \
                 """
                 SELECT table_name FROM information_schema.tables
-                            WHERE table_schema='public' AND table_name like '%0'
+                            WHERE table_schema='public' AND table_name like '%front' or table_name like '%top'
                             ORDER BY table_name DESC LIMIT 1
                 """
             print("tracking from 1 camera")
         else:
+            print("getting 2 tables from DB")
             query = \
             """
                 (SELECT table_name FROM information_schema.tables
-                            WHERE table_schema='public' AND table_name like '%0'
+                            WHERE table_schema='public' AND table_name like '%front'
                             ORDER BY table_name DESC LIMIT 1) 
                             UNION
                 (SELECT table_name FROM information_schema.tables
-                            WHERE table_schema='public' AND table_name like '%1'
+                            WHERE table_schema='public' AND table_name like '%top'
                             ORDER BY table_name DESC LIMIT 1)
             """
-            print("getting 2 most recent captures")
         cur.execute(query)
+       
         name = cur.fetchall()
+        if(cur.rowcount == 0):
+            print("ERROR - NO TABLE FOUND")
+            sys.exit()
 
     except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             sys.exit()
 
-    # we could start tread(s) here? 
-
-    tableNames = []    
     for item in enumerate(name):
         tableNames.append(item[1][0])
-        print(item[1][0])
 
-    #make 2D array to hold each item
-    #locations = [0*[2]] * len(tableNames) 
-    # locations = [[]]
-    locationGlobal = {'x':0, 'y':0, 'z':0}
-
-    #when we have 2 cameras
-    #camera 0 captures x,y - globally -> x,z
-    #camera 1 captures x,y - globally -> x,y
-    # CONFIRM THIS INFO ^^^^ 1/26/21
-    locationGlobal['x'] = locationGlobal['y'] = locationGlobal['z'] = 0
+    print("table name(s) are", tableNames)
 
     # setup init graph 
     fig = plt.figure(figsize=(10,10))
@@ -159,6 +260,7 @@ if __name__ == '__main__':
 
     #starts main loop of the program -> animate function does it all 
     # ani = FuncAnimation(fig, animate, interval=1000)
+    activelyTracked = []
     ani = FuncAnimation(fig, animate, fargs=(tableNames,),interval=100)
 
     #plt.tight_layout()
